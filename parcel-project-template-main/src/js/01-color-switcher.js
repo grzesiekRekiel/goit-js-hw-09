41f6a6da6885c3552e256094e7ccc6fc43fdcd43
rekielGrzegorz <rekielgrzegorz@gmail.com>
2024-09-04 20:01:21 +0200

    Zmieniono js

--- parcel-project-template-main/src/js/01-color-switcher.js
+++ parcel-project-template-main/src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonDataStart = document.querySelector("button[data-start]");
+const buttonDataStop = document.querySelector("button[data-stop]");
 
+buttonDataStop.disabled=true;
+let interwal;
+
+buttonDataStart.addEventListener("click",function(){
+    this.disabled=true;
+    buttonDataStop.disabled=false;
+    
+    
+    function changeColor(){function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    };
+    document.body.style.backgroundColor = getRandomHexColor();
+    
+}
+interwal = setInterval(changeColor,1000);
+});
+ 
+
+
+buttonDataStop.addEventListener("click",function(){
+    this.disabled=true;
+    buttonDataStart.disabled=false;
+    clearInterval(interwal);
+    
+});
